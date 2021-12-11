81351aa9578eed877caaa2586a8a70311518242d
man <1748719940@qq.com>
2021-12-11 21:18:42 +0800

    feat:my nevim conig

---
+++ init.vim
@@ -0,0 +1,10 @@
+
+" 基础设置
+lua require('basic')
+" Packer插件管理
+lua require('plugins')
+" 插件配置
+lua require('plugin-config/treesitter')
+set guifont=DroidSansMono_Nerd_Font:h11
+map <F3> :NERDTreeToggle<CR>
+map <leader>f :Telescope find_files<CR>
---
+++ lua/plugins.lua
@@ -0,0 +1,42 @@
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -------------------------- plugins -------------------------------------------
+  use {
+    "ellisonleao/gruvbox.nvim",
+    requires = {"rktjmp/lush.nvim"}
+    }
+  use {
+    "ryanoasis/vim-devicons",
+    "tiagofumo/vim-nerdtree-syntax-highlight"
+  }  
+  -- nerd-tree
+  use {
+    'preservim/nerdtree',
+  }
+  use {
+  'romgrk/barbar.nvim',
+  requires = {'kyazdani42/nvim-web-devicons'}
+  }
+  -- treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
+
+  use {
+  'nvim-telescope/telescope.nvim',
+  requires = { {'nvim-lua/plenary.nvim'} }
+}
+-- markdown preview
+  use{
+    'iamcco/markdown-preview.nvim'
+  }
+  use{
+    "vim-airline/vim-airline",
+    "vim-airline/vim-airline-themes"
+  }
+end)
+
+
